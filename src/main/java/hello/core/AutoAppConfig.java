98df90bb6a62a0c00c970c95ad99d45cfb162c87
JJONG\ljm17 <ljm920604@gmail.com>
2025-03-07 14:13:35 +0900

    Feat. 컴포넌트 스캔, 필터 사용 방법, 탐색 위치 사용방법

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,16 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+//        basePackages = "hello.core.member",
+//        basePackageClasses = AutoAppConfig.class
+        // 지정안하면 hello.core 부터 시작해서 하위 패키지는 다 찾음. ComponentScan 가 붙은 패키지가 시작 위치 관례는 설정 정보 클래스의 위츠를 프로젝트 최상단
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+
+}
---
+++ src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -0,0 +1,21 @@
+package hello.core.scan;
+
+import hello.core.AutoAppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AutoAppConfigTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+
+    @Test
+    void basicScan() {
+
+        MemberService memberService = ac.getBean(MemberService.class);
+
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+
+    }
+}
