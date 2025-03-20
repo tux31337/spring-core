3e5968567c4a2181e118658fdd1774f59cb40ef0
JJONG\ljm17 <ljm920604@gmail.com>
2025-03-20 15:55:13 +0900

    Feat.해결1 -  Application 를 직접 주입받아 사용할 때마다 다시 요청하기

--- src/test/java/hello/core/scope/SignletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SignletonWithPrototypeTest1.java
@@ -5,7 +5,9 @@ import jakarta.annotation.PreDestroy;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -35,21 +37,21 @@ public class SignletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
 
 
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean; // 생성시점에 주입됨...
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private ApplicationContext ac;
+
+
 
         public int logic() {
+            PrototypeBean prototypeBean = ac.getBean(PrototypeBean.class);
             prototypeBean.addCount();
             return prototypeBean.getCOUNT();
         }
